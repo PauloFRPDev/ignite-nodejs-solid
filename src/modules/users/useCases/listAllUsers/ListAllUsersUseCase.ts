8e02eb9fd04aba5cea023bc464eab6be0ad7001f
Paulo Felippe <paulofelipperp.dev@gmail.com>
2021-05-31 18:39:30 -0300

    feature: challenge done

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,17 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists");
+    }
+
+    if (!user.admin) {
+      throw new Error("You must be admin to retrieve the users list");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
